7ab858568cef7b465dc4f0488441795c1628d070
Julio Antonio Ortiz Pol <jortiz@contractor.appsbees.com>
2022-08-07 09:33:51 -0400

    created Header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <div class="header">
+        <div>Inclusion FactoryFour Apis status</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
