b290802025c5ba921bd180f58b1b5a0de6c30855
Julio Antonio Ortiz Pol <jortiz@contractor.appsbees.com>
2022-08-08 08:03:42 -0400

    show time on human readable way

--- src/App.js
+++ src/App.js
@@ -26,9 +26,32 @@ const apis = [
   { name: "users" },
   { name: "workflows" },
 ];
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
 
 const getStatusHealthEndPoint = (apiName) =>
   `https://api.factoryfour.com/${apiName}/health/status`;
+
+function convertMsToHM(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+
+    seconds = seconds % 60;
+    // 👇️ if seconds are greater than 30, round minutes up (optional)
+    minutes = seconds >= 30 ? minutes + 1 : minutes;
+
+    minutes = minutes % 60;
+
+    // 👇️ If you don't want to roll hours over, e.g. 24 to 00
+    // 👇️ comment (or remove) the line below
+    // commenting next line gets you `24:00:00` instead of `00:00:00`
+    // or `36:15:31` instead of `12:15:31`, etc.
+    hours = hours % 24;
+
+    return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+  }
 
 function App() {
   const [apiStatusList, setApiStatusList] = useState([]);
@@ -44,7 +67,10 @@ function App() {
         if (response.status === "fulfilled") {
           return {
             name: apis[index].name,
-            ...response.value.data,
+            time: convertMsToHM(response.value.data.time),
+            success: response.value.data.success,
+            message: response.value.data.message,
+            hostname: response.value.data.hostname
           };
         } else {
           return {
